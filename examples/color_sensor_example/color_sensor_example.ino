35bd36377e9921713ce550ac12c356f71a0a5a09
haunt07 <fachri111@gmail.com>
2026-03-01 20:57:37 +0700

    add calibration color

--- examples/color_sensor_example/color_sensor_example.ino
+++ examples/color_sensor_example/color_sensor_example.ino
@@ -1,21 +1,37 @@
 #include <color_sensor.h>
 
 ColorSensor colorSens;
-void setup(){
+Color colorRed(150, 255, 0, 90, 0, 90);
+Color colorBlue(0, 90, 0, 90, 150, 255);
+Color colorBlack(110, 255, 110, 255, 110, 255);
+
+void setup()
+{
     Serial.begin(115200);
-    colorSens.begin(A1,A2,A3,A4,A5);
+    colorSens.add(colorRed);
+    colorSens.add(colorBlue);
+    colorSens.add(colorBlack);
+    colorSens.begin(A1, A2, A3, A4, A5);
     Serial.println("Inisialisasi sistem");
 }
 
-void loop(){
-    ColorSensor::Color color = colorSens.readColor();
-    if(color == ColorSensor::Color::Red){
+void loop()
+{
+    Color color = colorSens.readColor();
+    if (color == colorRed)
+    {
         Serial.println("merah");
-    }else if(color == ColorSensor::Color::Blue){
+    }
+    else if (color == colorBlue)
+    {
         Serial.println("biru");
-    }else if(color == ColorSensor::Color::Black){
+    }
+    else if (color == colorBlack)
+    {
         Serial.println("hitam");
-    }else{
+    }
+    else
+    {
         Serial.println("warna lain");
     }
     delay(100);
--- src/color_sensor.h
+++ src/color_sensor.h
@@ -1,34 +1,78 @@
 #ifndef COLOR_SENSOR_H
 #define COLOR_SENSOR_H
-#include<Arduino.h>
+#include <Arduino.h>
 
-class ColorSensor{
-    private:
+class Color
+{
+public:
+    uint8_t redMin;
+    uint8_t redMax;
+    uint8_t greenMin;
+    uint8_t greenMax;
+    uint8_t blueMin;
+    uint8_t blueMax;
+    Color(
+        uint8_t redMin,
+        uint8_t redMax,
+        uint8_t greenMin,
+        uint8_t greenMax,
+        uint8_t blueMin,
+        uint8_t blueMax)
+    {
+        this->redMin = redMin;
+        this->redMax = redMax;
+        this->greenMin = greenMin;
+        this->greenMax = greenMax;
+        this->blueMin = blueMin;
+        this->blueMax = blueMax;
+    }
+
+    bool operator==(const Color &other) const;
+};
+
+struct ColorRGB
+{
+    uint16_t red;
+    uint16_t green;
+    uint16_t blue;
+};
+
+class ColorSensor
+{
+private:
     uint8_t pinS0;
     uint8_t pinS1;
     uint8_t pinS2;
     uint8_t pinS3;
     uint8_t pinOutput;
 
-    uint16_t rMin=120;
-    uint16_t gMin=130;
-    uint16_t bMin=110;
+    uint16_t rMin = 120;
+    uint16_t gMin = 130;
+    uint16_t bMin = 110;
 
-    uint16_t rMax=1800;
-    uint16_t gMax=1900;
-    uint16_t bMax=2000;
+    uint16_t rMax = 1800;
+    uint16_t gMax = 1900;
+    uint16_t bMax = 2000;
 
-    uint16_t rangeColorParam = 20;
-
-    uint16_t readSensor(bool valueS2, bool valueS3 );
+    uint16_t readSensor(bool valueS2, bool valueS3);
     uint8_t readRed();
     uint8_t readGreen();
     uint8_t readBlue();
+    struct NodeColor
+    {
+        Color *color;
+        NodeColor *next;
+    };
 
-    public:
-    enum class Color{Red, Blue, Black, Other};
-    void begin(uint8_t pinS0,uint8_t pinS1,uint8_t pinS2,uint8_t pinS3,uint8_t pinOutput);
+    NodeColor *firstColor = nullptr;
+    NodeColor *lastColor  = nullptr;
+
+public:
     Color readColor();
+    static Color unknownColor;
+    void add(Color &color);
+    void begin(uint8_t pinS0, uint8_t pinS1, uint8_t pinS2, uint8_t pinS3, uint8_t pinOutput);
+    ColorRGB readRGB();
 };
 
 #endif
